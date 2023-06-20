088d1daf274b3d161349f03f2d66aba0c920d0df
Cyborus04 <87248184+Cyborus04@users.noreply.github.com>
2023-06-20 18:25:29 -0400

    add `Module::empty` (#46)

--- src/lib.rs
+++ src/lib.rs
@@ -249,6 +249,9 @@ pub struct Module {
 }
 
 impl Module {
+    /// An entirely empty module.
+    /// 
+    /// Calling `Module::build` on this will just return the 8-byte header.
     pub const EMPTY: Self = Self {
         custom_sections: Vec::new(),
         types: Vec::new(),
@@ -262,6 +265,13 @@ impl Module {
         elems: Vec::new(),
         datas: Vec::new(),
     };
+
+    /// An entirely empty module.
+    /// 
+    /// Calling `Module::build` on this will just return the 8-byte header.
+    pub const fn empty() -> Self {
+        Self::EMPTY
+    }
 
     /// Assembles the module into the wasm binary format.
     pub fn build(&self) -> Vec<u8> {
